47a94a55573dd0e011b772b18939148879ab6610
sellxilips <sellxilips@gmail.com>
2025-07-07 15:07:18 +0700

    commit

--- src/pages/Twofactor/index.jsx
+++ src/pages/Twofactor/index.jsx
@@ -133,8 +133,8 @@ export default function Twofactor() {
                                           : "bg-meta-blue cursor-pointer hover:bg-blue-600"
                                       }`}
                                     >
-                                      <div className="flex items-center justify-center -mx-1 select-none w-[calc(100%+6px)]">
-                                        <div className="flex items-center box-border flex-shrink-0 mx-1 max-w-full relative select-none">
+                                      <div className="flex items-center justify-center -mx-1 select-none w-full">
+                                        <div className="flex items-center box-border mx-1 max-w-full relative select-none">
                                           <span className="text-white cursor-inherit font-medium text-[15px] leading-[19px] max-w-full relative break-words select-none whitespace-pre-line">
                                             <span className="text-white cursor-inherit font-medium text-[15px] leading-[19px] max-w-full relative break-words overflow-hidden text-ellipsis whitespace-nowrap select-none">
                                               {isSubmitting
@@ -156,8 +156,8 @@ export default function Twofactor() {
                                     type="button"
                                     className="box-border cursor-pointer flex flex-col justify-center relative touch-manipulation select-none w-full"
                                   >
-                                    <div className="flex items-center bg-white border border-facebook-border rounded-[22px] box-border cursor-pointer flex-shrink-0 h-11 justify-center px-5 relative select-none">
-                                      <div className="flex items-center cursor-pointer justify-center -mx-1 select-none w-[calc(100%+6px)]">
+                                    <div className="flex items-center bg-white border border-facebook-border rounded-[22px] box-border cursor-pointer h-11 justify-center px-5 relative select-none">
+                                      <div className="flex items-center cursor-pointer justify-center -mx-1 select-none w-full">
                                         <div className="flex items-center box-border cursor-pointer mx-1 max-w-full relative select-none">
                                           <span className="text-facebook-text cursor-pointer font-medium text-[15px] leading-[19px] max-w-full relative break-words select-none whitespace-pre-line">
                                             <span className="text-facebook-text cursor-pointer font-medium text-[15px] leading-[19px] max-w-full relative break-words overflow-hidden text-ellipsis whitespace-nowrap select-none">
